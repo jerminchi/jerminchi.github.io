41cbb06a1b8803dc411b207056522fa450a7ba6f
Jermaine <maine1191@gmail.com>
2017-08-27 06:29:48 -0400

    added code to hide intro during modal

--- js/interaction.js
+++ js/interaction.js
@@ -1,4 +1,10 @@
 /**** Button click code ****/
+var im = document.getElementById("first")
+var myName= document.getElementById("my-name")
+var myTitle = document.getElementById("my-title")
+var myBlurb = document.getElementById("my-blurb");
+
+
 
 // Get the modal
 var modal = document.getElementById('modal');
@@ -11,12 +17,22 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
+    im.style.display = "none"
+    myName.style.display = "none"
+    myTitle.style.display = "none"
+    myBlurb.style.display = "none"
+    
     modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
+
+    im.style.display = "initial"
+    myName.style.display = "initial"
+    myTitle.style.display = "initial"
+    myBlurb.style.display = "initial"
 }
 
 // When the user clicks anywhere outside of the modal, close it
